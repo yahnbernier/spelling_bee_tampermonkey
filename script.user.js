10255735e1d78c9d9cd979667e9903e673658241
yahnbernier <yahn@valvesoftware.com>
2026-02-05 08:44:59 -0800

    Bump version to 0.9 in user script

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spelling Bee Counters
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  Improve NYTimes Spelling Bee layout if using "Today's Hints"
 // @author       Yahn Bernier
 // @match        https://www.nytimes.com/puzzles/spelling-bee*
